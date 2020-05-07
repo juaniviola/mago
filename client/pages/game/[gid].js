f1cdb49857aba52da8b327c79d367889291bc60e
Juani Viola <juaniviola1297@gmail.com>
2020-05-07 14:02:03 -0300

    Initializing with logic

--- client/pages/game/[gid].js
+++ client/pages/game/[gid].js
@@ -11,10 +11,17 @@ class Game extends React.Component {
       username: null,
       socket: null,
       users: [],
-      show: true
+      show: true,
+      gameStarted: false,
+      myTurn: false,
+      cards: [],
+      mazo: [],
+      stack: ['-']
     }
     this.handleStart = this.handleStart.bind(this)
     this.handleSave = this.handleSave.bind(this)
+    this.handlePlayCard = this.handlePlayCard.bind(this)
+    this.handleTakeCard = this.handleTakeCard.bind(this)
   }
 
   async componentDidMount () {
@@ -31,7 +38,32 @@ class Game extends React.Component {
     })
 
     socket.on('card', data => {
-      console.log(data)
+      this.setState({ cards: data.cards })
+      if (this.state.users[data.turn] === this.state.username) {
+        this.setState({ myTurn: true })
+      }
+    })
+
+    socket.on('mazo', data => {
+      this.setState({ mazo: data.mazo, stack: data.stack, gameStarted: true })
+    })
+
+    socket.on('card_played', ({ card, turn }) => {
+      const stack = this.state.stack
+      stack.unshift(card)
+      this.setState({ stack })
+      if (this.state.users[turn] === this.state.username) {
+        this.setState({ myTurn: true })
+      }
+    })
+
+    socket.on('card_taked', i => {
+      const mazo = this.state.mazo
+      mazo.splice(0, 1)
+      this.setState({ mazo })
+      if (this.state.users[i] === this.state.username) {
+        this.setState({ myTurn: true })
+      }
     })
 
     let user = ''
@@ -45,6 +77,48 @@ class Game extends React.Component {
   handleStart () {
     const socket = this.state.socket
     socket.emit('start_match')
+  }
+
+  handlePlayCard (card) {
+    const c1 = card.slice(card.indexOf('_')+1, card.length)
+    const c2 = this.state.stack[0].slice(this.state.stack[0].indexOf('_')+1, this.state.stack[0].length)
+    const n1 = card.slice(0, card.indexOf('_'))
+    const n2 = this.state.stack[0].slice(0, this.state.stack[0].indexOf('_'))
+    if (c1 !== c2 && n1 !== n2) {
+      return alert('Jugada invalida!')
+    }
+
+    const socket = this.state.socket
+    const cards = this.state.cards
+    const stack = this.state.stack
+    cards.splice(cards.indexOf(card), 1)
+    stack.unshift(card)
+    this.setState({ cards, stack })
+    let i = this.state.users.indexOf(this.state.username)
+    if (n1 === '4' || n1 === '11') {
+      i=i+2
+    } else {
+      i++
+    }
+    const u = [...this.state.users, ...this.state.users]
+    if (u[i] !== this.state.username) this.setState({ myTurn: false })
+    socket.emit('play_card', { card, turn: this.state.users.indexOf(u[i]) })
+  }
+
+  handleTakeCard () {
+    const socket = this.state.socket
+    const cards = this.state.cards
+    const mazo = this.state.mazo
+    cards.push(mazo[0])
+    mazo.splice(0, 1)
+    this.setState({ cards, mazo, myTurn: false })
+    let i = this.state.users.indexOf(this.state.username)
+    if (i + 1 > (this.state.users.length)-1) {
+      i = 0
+    } else {
+      i++
+    }
+    socket.emit('take_card', i)
   }
 
   async handleSave () {
@@ -64,13 +138,24 @@ class Game extends React.Component {
       return <li key={i.toString()}>{el}</li>
     })
 
+    const renderCards = this.state.cards.map((el, i) => {
+      return (
+        <p key={i.toString()}>{el}
+          {this.state.myTurn && <button onClick={() => this.handlePlayCard(el)}>Jugar</button>}
+        </p>
+      )
+    })
+
     return (
       <Layout title="Game">
         <div style={{ textAlign: 'center' }}>
           <p>Game: {this.state.roomId}</p>
           <p>Conectados:</p>
           {renderUsers}
-          <button onClick={this.handleStart}>Comenzar</button>
+          {this.state.myTurn && <div><button onClick={this.handleTakeCard}>Alzar</button></div>}
+          stack: {this.state.stack[0]}
+          <div>{renderCards}</div>
+          {!this.state.gameStarted && <button onClick={this.handleStart}>Comenzar</button>}
         </div>
       </Layout>
     )
--- server/index.js
+++ server/index.js
@@ -18,6 +18,33 @@ io.on('connection', socket => {
   console.log('user connected with id:', socket.id)
 })
 
+function randomCards (users)  {
+  const cards = [
+    '1_oro', '2_oro', '3_oro', '4_oro', '5_oro', '6_oro', '7_oro', '10_oro', '11_oro', '12_oro',
+    '1_espada', '2_espada', '3_espada', '4_espada', '5_espada', '6_espada', '7_espada', '10_espada', '11_espada', '12_espada',
+    '1_basto', '2_basto', '3_basto', '4_basto', '5_basto', '6_basto', '7_basto', '10_basto', '11_basto', '12_basto',
+    '1_copa', '2_copa', '3_copa', '4_copa', '5_copa', '6_copa', '7_copa', '10_copa', '11_copa', '12_copa'
+  ]
+
+  const userCards = {}
+  for (let i in users) {
+    for (let j = 0; j < 5; j++) {
+      const x = Math.floor((Math.random() * (cards.length)))
+      if (j !== 0)
+        userCards[users[i]].push(cards[x])
+      else
+        userCards[users[i]] = [cards[x]]
+      cards.splice(x, 1)
+    }
+  }
+
+  const x = Math.floor((Math.random() * (cards.length)))
+  const stack = [ cards[x] ]
+  cards.splice(x, 1)
+
+  return { userCards, cards, stack }
+}
+
 const rooms = {}
 function listenRoom (room) {
   const _io = rooms[room]
@@ -38,12 +65,25 @@ function listenRoom (room) {
 
     socket.on('start_match', async () => {
       const users = await db.getUsers({ roomId: socket.nsp.name })
+      const _users = []
+      const _ids = []
       for (let i in users) {
         if (i%2 !== 0) {
-          _io.to(users[i]).emit('card', i)
+          _ids.push(users[i])
+        } else {
+          _users.push(users[i])
         }
       }
+
+      const rand = randomCards(_users)
+      for (let i in _ids) {
+        _io.to(_ids[i]).emit('card', { cards: rand.userCards[_users[i]], turn: 0 })
+      }
+      _io.emit('mazo', { mazo: rand.cards, stack: rand.stack })
     })
+
+    socket.on('play_card', ({ card, turn }) => socket.broadcast.emit('card_played', { card, turn }))
+    socket.on('take_card', i => socket.broadcast.emit('card_taked', i))
 
     socket.on('disconnect', async () => {
       try {
