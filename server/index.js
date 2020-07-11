b83a3518e0ea05ceedcf35ecb5475aa562623835
Juani Viola <juaniviola1297@gmail.com>
2020-07-11 18:41:00 -0300

    scaffolding improvement

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const io = require('socket.io')(http)
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const db = require('./db')
+const { randomCards, mixCards } = require('./utils/utils')
 
 // sv config
 app.use(cors())
@@ -17,46 +18,6 @@ app.use(express.json()) // Json parse
 io.on('connection', socket => {
   console.log('user connected with id:', socket.id)
 })
-
-function randomCards (users)  {
-  const cards = [
-    '1_oro', '2_oro', '3_oro', '4_oro', '5_oro', '6_oro', '7_oro', '10_oro', '11_oro', '12_oro',
-    '1_espada', '2_espada', '3_espada', '4_espada', '5_espada', '6_espada', '7_espada', '10_espada', '11_espada', '12_espada',
-    '1_basto', '2_basto', '3_basto', '4_basto', '5_basto', '6_basto', '7_basto', '10_basto', '11_basto', '12_basto',
-    '1_copa', '2_copa', '3_copa', '4_copa', '5_copa', '6_copa', '7_copa', '10_copa', '11_copa', '12_copa'
-  ]
-
-  const userCards = {}
-  for (let i in users) {
-    for (let j = 0; j < 5; j++) {
-      const x = Math.floor((Math.random() * (cards.length)))
-      if (j !== 0)
-        userCards[users[i]].push(cards[x])
-      else
-        userCards[users[i]] = [cards[x]]
-      cards.splice(x, 1)
-    }
-  }
-
-  const x = Math.floor((Math.random() * (cards.length)))
-  const stack = [ cards[x] ]
-  cards.splice(x, 1)
-
-  return { userCards, cards, stack }
-}
-
-function mixCards (stack) {
-  const nStack = []
-  const oStack = stack
-
-  for (let i in stack.length) {
-    const x = Math.floor((Math.random() * (cards.length)))
-    nStack.push(oStack[x])
-    oStack.splice(x, 1)
-  }
-
-  return nStack
-}
 
 const rooms = {}
 function listenRoom (room) {
---
+++ server/utils/cards.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const cards = [
+  '1_oro', '2_oro', '3_oro', '4_oro', '5_oro', '6_oro', '7_oro', '10_oro', '11_oro', '12_oro',
+  '1_espada', '2_espada', '3_espada', '4_espada', '5_espada', '6_espada', '7_espada', '10_espada', '11_espada', '12_espada',
+  '1_basto', '2_basto', '3_basto', '4_basto', '5_basto', '6_basto', '7_basto', '10_basto', '11_basto', '12_basto',
+  '1_copa', '2_copa', '3_copa', '4_copa', '5_copa', '6_copa', '7_copa', '10_copa', '11_copa', '12_copa'
+]
+
+module.exports = cards
