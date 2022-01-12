4e745192aad4964ce061d6a7bb902b6672897870
Juani Viola <juaniviola1297@gmail.com>
2022-01-12 11:02:30 -0300

    force option

--- server/db/index.js
+++ server/db/index.js
@@ -2,10 +2,10 @@ import sequelize from './setup';
 import { User, Room } from './models';
 import { UserApi, RoomApi } from './api';
 
-export default async () => {
+export default async ({ force = false } = {}) => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+    await sequelize.sync({ force });
 
     const api = {
       User: UserApi(User),
