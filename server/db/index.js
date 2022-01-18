b8297ed38c6f671867f9bd2d3be046c9da3a2f05
Juani Viola <juaniviola1297@gmail.com>
2022-01-18 14:57:35 -0300

    Singelton added to access to api

--- server/db/index.js
+++ server/db/index.js
@@ -3,23 +3,35 @@ import sequelize from './setup';
 import { Room } from './models';
 import { UserApi, RoomApi } from './api';
 
-export default async ({ force = false, redisUrl = null } = {}) => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync({ force });
-
-    const redisClient = redisUrl ? createClient({ url: redisUrl }) : createClient();
-    redisClient.on('error', (err) => console.log('redis conn error:', err));
-
-    await redisClient.connect();
-
-    const api = {
-      User: UserApi(redisClient),
-      Room: RoomApi(Room),
-    };
-
-    return api;
-  } catch (error) {
-    throw Error(error);
+class Database {
+  constructor() {
+    this.User = null;
+    this.Room = null;
   }
-};
+
+  async connect({ force = false, redisUrl = null } = {}) {
+    try {
+      await sequelize.authenticate();
+      await sequelize.sync({ force });
+
+      const redisClient = redisUrl ? createClient({ url: redisUrl }) : createClient();
+      redisClient.on('error', (err) => console.log('redis conn error:', err));
+
+      await redisClient.connect();
+
+      const api = {
+        User: UserApi(redisClient),
+        Room: RoomApi(Room),
+      };
+
+      this.User = api.User;
+      this.Room = api.Room;
+
+      return api;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+}
+
+export default new Database();
