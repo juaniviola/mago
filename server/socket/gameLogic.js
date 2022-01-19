b049ce51f07cfc318b79a1561eff44ecee1c6a14
Juani Viola <juaniviola1297@gmail.com>
2022-01-19 14:10:53 -0300

    socket logic game added

--- server/socket/gameLogic.js
+++ server/socket/gameLogic.js
@@ -1,6 +1,22 @@
+import cardUtils from '../utils';
+
+const { mixCards } = cardUtils;
+
 export default function (io, socket) {
-  socket.on('play_card', () => {});
-  socket.on('take_card', () => {});
-  socket.on('change_type', () => {});
-  socket.on('repartir', () => {});
+  socket.on('play_card', ({ card, turn, quantity }) => {
+    socket.broadcast.emit('card_played', { card, turn, quantity });
+  });
+
+  socket.on('take_card', ({ turn, quantity }) => {
+    socket.broadcast.emit('card_taked', { turn, quantity });
+  });
+
+  socket.on('change_type', (type) => {
+    socket.broadcast.emit('type_changed', type);
+  });
+
+  socket.on('shuffle', (stack) => {
+    const newStack = mixCards(stack);
+    io.emit('shuffled', newStack);
+  });
 }
