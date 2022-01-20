bb4804928b52e1974849528e619ff293a5964d90
Juani Viola <juaniviola1297@gmail.com>
2022-01-20 16:06:51 -0300

    database error fixed

--- server/socket/logic.js
+++ server/socket/logic.js
@@ -3,34 +3,31 @@ import database from '../db';
 import cardUtils from '../utils';
 
 const { randomCards } = cardUtils;
-const { User, Room } = database;
 
 export default function (io, socket) {
+  socket.on('msg', (msg) => io.emit('msg2', msg));
+
   socket.on('new_player', async ({ roomId, username }) => {
-    let user = null;
-    const getUser = await User.get({ roomId, username });
+    const getUser = await database.User.get({ roomId, username });
 
-    if (Object.keys(getUser || {}).length < 1) {
-      user = username;
-    } else {
-      user = username.concat(shortid.generate());
-    }
+    const user = (!getUser || getUser.length < 1)
+      ? username : username.concat('-'.concat(shortid.generate()));
 
-    await User.set({ roomId, username: user, socketId: socket.id });
+    await database.User.set({ roomId, username: user, socketId: socket.id });
 
     io.to(socket.id).emit('username', user);
     socket.broadcast.emit('user_connected', user);
   });
 
   socket.on('start_match', async (roomId) => {
-    await Room.updateStatus({ roomId, started: true });
-    const usersFromRoom = await User.getFromRoom(roomId);
+    await database.Room.updateStatus({ roomId, started: true });
+    const usersFromRoom = await database.User.getFromRoom(roomId);
 
     const users = Object.keys(usersFromRoom);
     const { userCards, cards, stack } = randomCards(users);
 
-    Object.entries(usersFromRoom).forEach(([user, socketId]) => {
-      io.to(socketId).emit('cards', userCards[user]);
+    users.forEach((user) => {
+      io.to(usersFromRoom[user]).emit('cards', userCards[user]);
     });
 
     io.emit('start_match', { users, stack, cards, turn: 0 });
@@ -41,20 +38,22 @@ export default function (io, socket) {
   socket.on('play_again', () => io.emit('new_game'));
 
   socket.on('disconnect', async () => {
-    const roomId = socket.nsp.name;
-    const usersFromRoom = await User.getFromRoom(roomId);
+    const roomId = (socket.nsp.name).slice(1);
+    const usersFromRoom = await database.User.getFromRoom(roomId);
 
     if (Object.keys(usersFromRoom || {}).length === 1) {
-      await Room.remove(roomId);
-      await User.removeOwner(roomId);
-      await User.remove({ roomId, username: Object.keys(usersFromRoom)[0] });
+      await database.Room.remove(roomId);
+      await database.User.removeOwner(roomId);
+      await database.User.remove({ roomId, username: Object.keys(usersFromRoom)[0] });
 
       return;
     }
 
-    const users = Object.keys(usersFromRoom)
-      .filter((username) => usersFromRoom[username] !== socket.id);
+    let users = Object.keys(usersFromRoom);
+    const [userToRemove] = users.filter((username) => usersFromRoom[username] === socket.id);
+    await database.User.remove({ roomId, username: userToRemove });
 
+    users = users.filter((username) => usersFromRoom[username] !== socket.id);
     io.emit('user_disconnected', users);
   });
 }
