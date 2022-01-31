398bf625aa7260adbce8f4091e152279b320568c
Juani Viola <juaniviola1297@gmail.com>
2022-01-31 14:28:40 -0300

    add options

--- client/components/gameId/connection.ts
+++ client/components/gameId/connection.ts
@@ -1,12 +1,17 @@
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 //TODO: import from config
-const server = 'ws://localhost:8001';
+const server = 'localhost:8001';
 
-export default function Connect(gameId: string, username: string): void {
-  const socket = io(server.concat(`/${gameId}`));
+export default function useConnect(gameId: string, username: string): void {
+  const socket = io(server.concat(`/${gameId}`), {
+    transports: ['websocket'],
+    forceNew: true,
+  });
 
-  socket.on('msg2', (msg: string) => console.log('>>', msg));
+  socket.on('msg2', (message) => {
+    console.log('>>', message);
+  });
 
   socket.emit('msg', 'holi');
 }
