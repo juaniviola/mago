cf914285f960ece1f7c39e7bb8561757990b365f
Juani Viola <juaniviola1297@gmail.com>
2022-01-31 14:28:57 -0300

    custom hook and use effect fix

--- client/pages/game/[gameId].tsx
+++ client/pages/game/[gameId].tsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import Connect from '../../components/gameId/connection';
+import useConnect from '../../components/gameId/connection';
 
 export default function Game(): JSX.Element {
   const router = useRouter();
   const [gameId, setGameId]: [string, any] = useState('');
 
-  useEffect((): void => {
+  useEffect(() => {
     setGameId(router.query.gameId);
 
-    //  FIXME: get username and change connect name
-    Connect(gameId, sessionStorage.getItem('username'));
-  }, []);
+    useConnect(gameId, sessionStorage.getItem('username'));
+  }, [gameId]);
 
   return (
     <div>
