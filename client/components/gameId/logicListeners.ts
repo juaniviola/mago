2b03b36b5540742c9aaa8de71b5fa75aeed64a74
Juani Viola <juaniviola1297@gmail.com>
2022-02-04 14:25:01 -0300

    user disconnected listener

--- client/components/gameId/logicListeners.ts
+++ client/components/gameId/logicListeners.ts
@@ -4,7 +4,11 @@ export default function logicListeners(
 ): void {
   socket.on('cards', (userCards: []) => {
     socket.on('start_match', ({ users, cards, stack, turn }) => {
-      setGameProps({ gameStarted: true, quantity: 0, userCards, users, cards, stack, turn });
+      setGameProps({ gameStarted: true, quantity: 0, userCards, users, cards, stack, turn, typeCard: stack[0].slice(3) });
     });
+  });
+
+  socket.on('user_disconnected', (users: []) => {
+    setGameProps({ users });
   });
 }
